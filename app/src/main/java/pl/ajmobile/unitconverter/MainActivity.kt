f679fe0b8727a2aea91db938ee2df86c8bcc124d
Agnieszka Janicka <agnieszka.janicka06@gmail.com>
2020-02-27 13:14:14 +0100

    step 2

--- app/src/main/java/pl/ajmobile/unitconverter/MainActivity.kt
+++ app/src/main/java/pl/ajmobile/unitconverter/MainActivity.kt
@@ -3,17 +3,18 @@ package pl.ajmobile.unitconverter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Modifier
+import androidx.compose.state
 import androidx.ui.core.Text
 import androidx.ui.core.TextField
 import androidx.ui.core.setContent
 import androidx.ui.graphics.Color
+import androidx.ui.input.KeyboardType
 import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.RadioButton
 import androidx.ui.material.RadioGroup
 import androidx.ui.material.surface.Surface
+import androidx.ui.text.TextFieldValue
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 
@@ -29,36 +30,55 @@ class MainActivity : AppCompatActivity() {
 @Preview
 @Composable
 fun UnitConverterUI() {
+    val units = state { TextFieldValue("20") }
+    val radioOptions = listOf(Unit.KG.unitName, Unit.OZ.unitName)
+    val (selectedOption, onOptionSelected) = state { radioOptions.first() }
+
+    val calculatedValue = state { 0f }
+
     MaterialTheme {
         Surface(color = Color.White) {
             Center {
                 FlowColumn(crossAxisAlignment = FlowCrossAxisAlignment.Center) {
                     Text(text = "Kalkulator wag")
-                    TextField(value = "tutaj wpisz wartość")
+                    TextField(
+                        keyboardType = KeyboardType.Number,
+                        value = units.value,
+                        onValueChange = { textFieldValue -> units.value = textFieldValue })
                     Spacer(modifier = LayoutHeight(20.dp))
                     Column {
-                        RadioGroup {
-                            Row {
-                                RadioButton(selected = false, onSelect = {})
-                                Text(text = "Kilogramy")
-                            }
-                            Row {
-                                RadioButton(selected = false, onSelect = {})
-                                Text(text = "Uncje")
-                            }
-                        }
+                        RadioGroup(
+                            options = radioOptions,
+                            selectedOption = selectedOption,
+                            onSelectedChange = onOptionSelected
+                        )
                     }
                     Spacer(modifier = LayoutHeight(20.dp))
-                    Button {
+                    Button(onClick = {
+                        calculatedValue.value = calculateValue(selectedOption, units.value.text)
+                    }) {
                         Text(text = "Przelicz")
                     }
 
                     Spacer(modifier = LayoutHeight(40.dp))
 
-                    Text(text = "Wartość: ")
+                    Text(text = "Wartość:  ${calculatedValue.value}")
                 }
             }
         }
+    }
+}
+
+fun calculateValue(unit: String, value: String): Float {
+    val floatValue = value.toFloat()
+    return when (unit) {
+        Unit.KG.unitName -> {
+            floatValue * 35.2739619f
+        }
+        Unit.OZ.unitName -> {
+            floatValue * 0.0283495231f
+        }
+        else -> 0f
     }
 }
 
---
+++ app/src/main/java/pl/ajmobile/unitconverter/Tools.kt
@@ -0,0 +1,6 @@
+package pl.ajmobile.unitconverter
+
+enum class Unit(val unitName: String){
+    KG("kilogramy"),
+    OZ("uncje")
+}
