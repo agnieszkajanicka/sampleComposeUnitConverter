71f186e192dbc93e05a6c71def7030e1a3c313e5
Agnieszka Janicka <agnieszka.janicka06@gmail.com>
2020-02-27 12:45:25 +0100

    basic layout

--- app/src/main/java/pl/ajmobile/unitconverter/MainActivity.kt
+++ app/src/main/java/pl/ajmobile/unitconverter/MainActivity.kt
@@ -3,17 +3,60 @@ package pl.ajmobile.unitconverter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.core.Text
+import androidx.ui.core.TextField
 import androidx.ui.core.setContent
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
+import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.RadioButton
+import androidx.ui.material.RadioGroup
+import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
+            UnitConverterUI()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UnitConverterUI() {
+    MaterialTheme {
+        Surface(color = Color.White) {
+            Center {
+                FlowColumn(crossAxisAlignment = FlowCrossAxisAlignment.Center) {
+                    Text(text = "Kalkulator wag")
+                    TextField(value = "tutaj wpisz wartość")
+                    Spacer(modifier = LayoutHeight(20.dp))
+                    Column {
+                        RadioGroup {
+                            Row {
+                                RadioButton(selected = false, onSelect = {})
+                                Text(text = "Kilogramy")
+                            }
+                            Row {
+                                RadioButton(selected = false, onSelect = {})
+                                Text(text = "Uncje")
+                            }
+                        }
+                    }
+                    Spacer(modifier = LayoutHeight(20.dp))
+                    Button {
+                        Text(text = "Przelicz")
+                    }
+
+                    Spacer(modifier = LayoutHeight(40.dp))
+
+                    Text(text = "Wartość: ")
+                }
             }
         }
     }
@@ -21,10 +64,10 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", style = MaterialTheme.typography().subtitle1)
 }
 
-@Preview
+//@Preview
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
